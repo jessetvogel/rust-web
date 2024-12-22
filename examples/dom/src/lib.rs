4342d95dbf66b1cc671f7f0f29a3feac1bbdcf46
Andreas Tzionis <andy0n7@gmail.com>
2024-12-22 11:13:20 +0200

    minor fix

--- examples/dom/src/lib.rs
+++ examples/dom/src/lib.rs
@@ -7,12 +7,12 @@ pub fn main() {
 
     std::panic::set_hook(Box::new(|e| { Js::invoke("console.log({})", &[Str(e.to_string())]); }));
 
-    let button = Js::invoke("return document.createElement({})", &[Str("button".into())]).to_ref().unwrap();
+    let button = Js::invoke("return document.createElement('button')", &[]).to_ref().unwrap();
     Js::invoke("{}.textContent = 'Click'", &[Ref(button)]);
 
     let function_ref = create_callback(move |_s| { Js::invoke("alert('hello')", &[]); });
     Js::invoke("{}.addEventListener('click',{})", &[Ref(button), Ref(function_ref)]);
 
-    let body = Js::invoke("return document.querySelector({})", &[Str("body".into())]).to_ref().unwrap();
+    let body = Js::invoke("return document.querySelector('body')", &[]).to_ref().unwrap();
     Js::invoke("{}.appendChild({})", &[Ref(body), Ref(button)]);
 }
