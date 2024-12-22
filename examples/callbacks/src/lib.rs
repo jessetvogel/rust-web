905447ce071b06b21661ccbc8481c1df08f6693b
Andreas Tzionis <andy0n7@gmail.com>
2024-12-22 11:58:42 +0200

    added callbacks example

---
+++ examples/callbacks/src/lib.rs
@@ -0,0 +1,28 @@
+
+use tinyweb::callbacks::{create_callback, create_future_callback};
+use tinyweb::runtime::{Runtime, RuntimeFuture};
+use tinyweb::invoke::*;
+
+#[no_mangle]
+pub fn main() {
+
+    std::panic::set_hook(Box::new(|e| { Js::invoke("console.log({})", &[Str(e.to_string())]); }));
+
+    // invoke
+    Js::invoke("console.log('invoke')", &[]);
+
+    // callback
+    let function_ref = create_callback(move |_s| { Js::invoke("console.log('invoke timer')", &[]); });
+    Js::invoke("setTimeout({}, 1000)", &[ Ref(function_ref)]);
+
+    // future callback
+    // TODO
+
+    Runtime::block_on(async move {
+        let future = RuntimeFuture::<()>::new();
+        let callback_ref = create_future_callback(future.id());
+        Js::invoke("window.setTimeout({},2000)", &[Ref(callback_ref)]);
+        future.await;
+        Js::invoke("console.log('invoke timer future')", &[]);
+    });
+}
