a76c59e4809ec75ebb43908aeeb7bbedadebe3de
Andreas Tzionis <andy0n7@gmail.com>
2024-12-22 12:57:47 +0200

    added async example

--- examples/callbacks/src/lib.rs
+++ examples/callbacks/src/lib.rs
@@ -11,16 +11,29 @@ pub fn main() {
     // invoke
     Js::invoke("console.log('invoke')", &[]);
 
-    // callback
+    // invoke callback
     let function_ref = create_callback(move |_s| { Js::invoke("console.log('invoke timer')", &[]); });
     Js::invoke("setTimeout({}, 1000)", &[ Ref(function_ref)]);
 
-    // future callback
+    // invoke future callback
     let future = RuntimeFuture::<()>::new();
     let callback_ref = create_future_callback(future.id());
     Js::invoke("window.setTimeout({},2000)", &[Ref(callback_ref)]);
     Runtime::block_on(async move {
         future.await;
         Js::invoke("console.log('invoke timer future')", &[]);
+    });
+
+    // invoke async callback
+    let url = "https://pokeapi.co/api/v2/pokemon/1";
+    let future = RuntimeFuture::<String>::new();
+    let future_id = future.id();
+    let callback_ref = create_callback(move |e| {
+        let result = Js::invoke("return objects[{}].name", &[Str(e.to_string())]).to_str().unwrap();
+        RuntimeFuture::wake(future_id, result);
+    });
+    Js::invoke("fetch({}).then(r => r.json()).then(r => { {}(r) })", &[Str(url.into()), Ref(callback_ref)]);
+    Runtime::block_on(async move {
+        Js::invoke("console.log('invoke fetch', {})", &[Str(future.await)]);
     });
 }
